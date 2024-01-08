4e066874c6b04621fdc900845a09f46a097fd1e1
ISTJ <73210774+Gleam0807@users.noreply.github.com>
2024-01-08 21:17:18 +0900

    Update 피자나눠먹기2.swift

--- 프로그래머스/unrated/피자나눠먹기2.swift
+++ 프로그래머스/unrated/피자나눠먹기2.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ n: Int) -> Int {
-    return (1...).first { (6 * $0) % n == 0 }! // filter : 만족하는 요소만 모아서 return 즉 6 * $0 % n == 0을 만족하는 요소만 모아서 반환해줍니다
+    return (1...).first { (6 * $0) % n == 0 }! // first : 만족하는 첫번째 요소 tuple로 return
 }
